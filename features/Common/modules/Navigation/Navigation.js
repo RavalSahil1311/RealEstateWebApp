73e6cb330864692df0f297951a42229d738a820d
SahilRaval <ravalsahil1311@gmail.com>
2023-05-10 18:28:01 +0530

    WorkingV1

---
+++ features/Common/modules/Navigation/Navigation.js
@@ -0,0 +1,8 @@
+import NavigationDesktop from "./components/NavigationDesktop/NavigationDesktop";
+import NavigationMobile from "./components/NavigationMobile/NavigationMobile";
+const Navigation = () => {
+    return <><NavigationDesktop />
+        <NavigationMobile />   </>;
+}
+
+export default Navigation;
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Navigation from "@/features/Common/modules/Navigation/Navigation";
+
+                      //  features\Common\modules\Navigation\Navigation.js
+export default function Home() {
+  return (
+    <>
+      <Navigation />
+    </>
+    );
+}
