aa05d6dda78699c9424974fd72c49522f6581e69
SahilRaval <ravalsahil1311@gmail.com>
2023-05-11 21:24:48 +0530

    WorkingV2

---
+++ features/Common/modules/Navigation/NavigationConsts.js
@@ -0,0 +1,11 @@
+import { HiPhoneArrowDownLeft } from "react-icons/hi2";
+
+export const navigationLinks = [
+  { title: "Properties", link: "/Priperties" },
+  { title: "Contact", link: "/contact" },
+  {
+    title: "(0278) 111 111",
+    link: "tel:(0278) 111 111",
+    icon: <HiPhoneArrowDownLeft />,
+  },
+];
--- features/Common/modules/Navigation/components/NavigationDesktop/NavigationDesktop.js
+++ features/Common/modules/Navigation/components/NavigationDesktop/NavigationDesktop.js
@@ -1,5 +1,45 @@
-const NavigationDesktop = () =>{
-    return <>NavigationDesktop</>;
-}
+import { Box, Button, Center, Flex, Text } from "@chakra-ui/react";
+import Link from "next/link";
+import { HiHomeModern } from "react-icons/hi2";
+import { navigationLinks } from "../../NavigationConsts";
+
+const NavigationDesktop = () => {
+  return (
+    <Box
+      color="blue.600"
+      paddingY="2rem"
+      backgroundColor="white"
+      display={{ base: "none", md: "block" }}
+    >
+      <Box maxWidth="1280px" margin="0 auto">
+        <Flex alignItems="center" justifyContent="space-between">
+          <Link href="/">
+            <Box display="flex" gap="2" alignItems="Center">
+              <HiHomeModern size="30" />
+              <Text fontSize="2xl" fontWeight="black">
+                HAPPY HOME
+              </Text>
+            </Box>
+          </Link>
+          {navigationLinks.map((item) => (
+            <NavigationLink key={item.title} {...item} />
+          ))}
+          <Button>CREATE LISTING</Button>
+        </Flex>
+      </Box>
+    </Box>
+  );
+};
 
 export default NavigationDesktop;
+
+const NavigationLink = ({ title, link, icon }) => {
+  return (
+    <Link href={link}>
+      <Flex alignItems="cente" gap="0.5rem">
+        {icon}
+        {title}
+      </Flex>
+    </Link>
+  );
+};
